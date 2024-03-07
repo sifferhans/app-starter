7cc8a3f51639eb85969304eea1dac95abb66b0ed
Sigve Hansen <sighan12@gmail.com>
2024-03-07 11:30:58 +0100

    fix: use nuxt ui navigation component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,38 @@
 <script lang="ts" setup>
 const { logout, user } = useAuth();
+
+const links = computed(() =>
+  [
+    {
+      label: "Admin",
+      to: "/admin",
+      icon: "i-heroicons-lock-closed",
+      enabled: user.value?.role === "admin",
+    },
+    {
+      label: "Dashboard",
+      to: "/",
+    },
+  ].filter((link) => ("enabled" in link ? link.enabled : true))
+);
 </script>
 
 <template>
   <div>
     <header
-      class="p-4 bg-white border-b border-b-gray-200 dark:border-b-gray-800"
+      class="bg-white border-b border-b-gray-200 dark:border-b-gray-800 relative"
     >
-      <UContainer>
-        <nav class="flex gap-4 justify-between items-center">
-          <NuxtLink to="/" class="font-bold">App</NuxtLink>
-          <div class="flex gap-4 items-center">
-            <small>{{ user.email }}</small>
-            <UButton @click="logout" variant="solid" color="white" size="xs">
-              Log out
-            </UButton>
-          </div>
-        </nav>
+      <UContainer class="flex items-center justify-between gap-6">
+        <NuxtLink to="/" class="font-bold">App</NuxtLink>
+
+        <UHorizontalNavigation :links="links" />
+
+        <div v-if="user" class="flex gap-4 items-center">
+          <small>{{ user.email }}</small>
+          <UButton @click="logout" variant="solid" color="white" size="xs">
+            Log out
+          </UButton>
+        </div>
       </UContainer>
     </header>
 
