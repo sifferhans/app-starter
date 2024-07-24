4daa628142c560ff7804fbc2ab4a9d7badc9a02d
Sigve Hansen <sighan12@gmail.com>
2024-07-24 17:52:55 +0200

    chore(drizzle): update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  driver: "libsql",
+export default defineConfig({
+  dialect: "sqlite",
   schema: "./server/db/schema.ts",
   out: "./server/db/migrations",
   dbCredentials: {
     url: "file:./server/database.db",
   },
-} satisfies Config;
+});
